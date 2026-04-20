bd9883766fde9106d746ae942633e01e2da206eb
Pavinesh24 <pavineshdhinesh@gmail.com>
2026-04-20 17:23:49 +0530

    Add files via upload

---
+++ counter.svelte
@@ -0,0 +1,24 @@
+<script>
+var count=0
+function increase()
+{
+count=count+1
+}
+function decrease()
+{
+count=count-1
+}
+function reset()
+{
+count=0
+}
+</script>
+
+<h1>Counter</h1>
+
+<h1>{count}</h1>
+<button on:click={increase}>+</button>
+<button on:click={decrease}>-</button>
+<button on:click={reset}>Reset</button>
+
+
