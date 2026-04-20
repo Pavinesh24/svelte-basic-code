0c60b5e1e68402caa2c71072f0fe1c0d53a39d67
Pavinesh24 <pavineshdhinesh@gmail.com>
2026-04-20 17:41:40 +0530

    Add files via upload

---
+++ passwordstrength.svelte
@@ -0,0 +1,22 @@
+<script>
+var password=""
+var strength=""
+function strength()
+{
+if(password.length >10 && /\d/.test(password))
+{
+strength="Strong"
+}
+else if(password.length>=6)
+{
+strength="Medium"
+}
+else
+{
+strength="Weak"
+}
+}
+</script>
+<h1>Enter your password</h1>
+<input type="password" bind:value={password} on:input={checkStrength} placeholder="Enter password"/>
+<h2>{strength}</h2>
